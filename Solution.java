efa0e84515d775dd96651c5808400ea80b0ec727
Justin Butler <57680670+JustinButler700@users.noreply.github.com>
2021-11-20 19:38:53 -0800

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,11 +1,8 @@
 //Justin Butler
 // 11/20/2021
-// This one is a pretty easy one.
+// This one is a pretty easy one lol.
 class Solution {
     public int strStr(String haystack, String needle) {
-        //Get the index location of our substring
-        int strIndex = haystack.indexOf(needle);
-        //If we find a valid substring index, return it. else return -1.
-        return strIndex>=0 ? strIndex : -1;
+        return haystack.indexOf(needle);
     }
 }
